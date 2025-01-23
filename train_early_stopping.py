cd15c58380102d9e9b4cfc2dc461d71c8ece1826
sinamei <57291022+sinamei@users.noreply.github.com>
2025-01-23 16:50:59 +0100

    add multi-GPU training and mixed precision

--- train_early_stopping.py
+++ train_early_stopping.py
@@ -84,11 +84,13 @@ def train():
 
     # 5) Setup trainer
     trainer = pl.Trainer(
-        max_epochs=100,             # set to a higher number; early stopping may stop earlier
+        max_epochs=100,
+        strategy='ddp',# set to a higher number; early stopping may stop earlier
         accelerator='gpu' if torch.cuda.is_available() else 'cpu',
-        devices=1,
+        devices=2,
+        precision = '16-mixed',
         callbacks=[early_stopping_callback, checkpoint_callback],
-        log_every_n_steps=1
+        log_every_n_steps=100
     )
 
     # 6) Train the model
