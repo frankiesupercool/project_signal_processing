63b2e9eb9aed9f4e4d1735e7ef12cd0a0a2e9dd1
frankie <franzi.pph@googlemail.com>
2025-02-10 14:15:52 +0100

    test cuda name

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ upsample_factor = 3.2
 upsampled_sample_rate = int(sample_rate * upsample_factor)
 
 # trainer
-gpus = [1]
+gpus = ['cuda:1']
 max_epochs = 100
 
 # root checkpoint save - public available checkpoints folder on sppc25
