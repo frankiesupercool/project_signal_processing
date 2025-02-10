1b92331e2e6b229c43ac63921a43836dd24ede65
frankie <franzi.pph@googlemail.com>
2025-02-10 23:40:41 +0100

    use public checkpoint folder again

--- config.py
+++ config.py
@@ -46,4 +46,4 @@ gpus = [1]
 max_epochs = 100
 
 # root checkpoint save - public available checkpoints folder on sppc25
-root_checkpoint = ".."
+root_checkpoint = "../../../../data/datasets/checkpoints/sp2025"
